e0712ef8cee23e88f4895668333d88c17efbf18b
Helen <ladyl3na@gmail.com>
2018-04-23 21:49:25 -0400

    even more backups

---
+++ nav/Assets/nav/NPC.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPC : MonoBehaviour {
+	public Transform target;
+	NavMeshAgent agent;
+
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+		agent.SetDestination(target.position);
+
+	}
+}
